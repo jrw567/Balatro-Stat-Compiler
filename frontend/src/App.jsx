2714acbc659395489cf904b3b5300ae1d2205ec9
Jay <jayrobertwhitaker@gmail.com>
2025-07-21 15:04:56 -0400

    Added error handling for submitting form without uploading file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ function App() {
       <form action={uploadFile}>
         <input type="file" name="profile" accept=".jkr"/>
         <input type="submit" />
+        <br />
+        <span id='file1'></span>
       </form>
     </>
   )
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,8 @@
 export const uploadFile = async data => {
-    await fetch("http://127.0.0.1:5000/", {method: "POST", body: data});
+    if(data.entries().next().value[1].size != 0){
+        let response = await fetch("http://127.0.0.1:5000/", {method: "POST", body: data}).then((rsp) => rsp.json());
+        document.getElementById("file1").innerHTML = response.message;
+    } else {
+        document.getElementById("file1").innerHTML = "Please submit a profile.jkr file";
+    }
 }
