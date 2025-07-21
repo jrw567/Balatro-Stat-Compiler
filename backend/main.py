55bf70d2b76be053d16bfacfa44ac4478ec58edb
Jay <jayrobertwhitaker@gmail.com>
2025-07-21 13:13:59 -0400

    Added File Upload functionality to successfully read on backend

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import zlib
 
 #create the app
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def temp():
-    return True
+    file_data = request.files.get("profile").read()
+    print(zlib.decompress(file_data, wbits= -zlib.MAX_WBITS))
+    return jsonify({"message": "File Uploaded Successfully"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,10 @@ function App() {
 
   return (
     <>
-      
+      <form action="http://localhost:5000" method="POST" encType="multipart/form-data">
+        <input type="file" name="profile" accept=".jkr"/>
+        <input type="submit" />
+      </form>
     </>
   )
 }
