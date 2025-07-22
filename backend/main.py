59f8deefb0457b22fe664b22b062354cac3f81f2
Jay <jayrobertwhitaker@gmail.com>
2025-07-22 17:16:02 -0400

    Added config file and setup about 1/3 of savefile model for SQLALCHEMY

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,16 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import request, jsonify
+from config import app, db
+from models import SaveFile
 import zlib, json
-
-#create the app
-app = Flask(__name__)
-CORS(app)
 
 @app.route("/", methods=["POST"])
 def readFile():
     if(request.files.get("profile").filename != "profile.jkr"):
         return jsonify({"message": "Please upload a profile.jkr file"}), 400
+    
     file_data = request.files.get("profile").read()
     file_data = zlib.decompress(file_data, wbits= -zlib.MAX_WBITS).decode("utf-8")
+
     if "career_stats" not in file_data or "joker_usage" not in file_data or "voucher_usage" not in file_data or "challenge_progress" not in file_data or "consumeable_usage" not in file_data:
         return jsonify({"message": "profile.jkr file missing required information"}), 400
     
@@ -21,6 +20,7 @@ def readFile():
     file_data = file_data.replace(",}", "}")
     file_data = file_data.replace("return ", "")
 
+    # want to replace this to read in wins and losses for each joker and combine each stake into a total number of wins and losses
     file_data = file_data.replace("1:", '"white_stake":')
     file_data = file_data.replace("2:", '"red_stake":')
     file_data = file_data.replace("3:", '"green_stake":')
@@ -31,7 +31,7 @@ def readFile():
     file_data = file_data.replace("8:", '"gold_stake":')
 
     file_data = json.loads(file_data)
-    # print(file_data["career_stats"])
+    print(file_data)
     return jsonify({"message": "File Uploaded Successfully"}), 200
 
 if __name__ == "__main__":
