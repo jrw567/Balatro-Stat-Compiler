bcd8e7246663922f038c80fae7985951c9049432
Jay <jayrobertwhitaker@gmail.com>
2025-07-24 11:29:50 -0400

    Added functionality to read career data into SQLALCHEMY

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,17 @@
 from flask import request, jsonify
 from config import app, db
-from models import Career,Joker
+from models import Career,Joker,Consumeable,Voucher,Deck
 import zlib, json
 
-@app.route("/", methods=["POST"])
-def readFile():
+@app.route("/upload_file/<int:file_number>", methods=["POST"])
+def readFile(file_number):
     if(request.files.get("profile").filename != "profile.jkr"):
         return jsonify({"message": "Please upload a profile.jkr file"}), 400
     
     file_data = request.files.get("profile").read()
     file_data = zlib.decompress(file_data, wbits= -zlib.MAX_WBITS).decode("utf-8")
 
-    if "career_stats" not in file_data or "joker_usage" not in file_data or "voucher_usage" not in file_data or "challenge_progress" not in file_data or "consumeable_usage" not in file_data:
+    if "career_stats" not in file_data or "joker_usage" not in file_data or "voucher_usage" not in file_data or "challenge_progress" not in file_data or "consumeable_usage" not in file_data or "deck_usage" not in file_data:
         return jsonify({"message": "profile.jkr file missing required information"}), 400
     
     #removes unecessary characters to enable json conversion
@@ -21,17 +21,18 @@ def readFile():
     file_data = file_data.replace(",}", "}")
     file_data = file_data.replace("return ", "")
 
-    # replaces difficulty naming convention in save file to a readable format to enable json conversion
-    file_data = file_data.replace("1:", '"white_stake":')
-    file_data = file_data.replace("2:", '"red_stake":')
-    file_data = file_data.replace("3:", '"green_stake":')
-    file_data = file_data.replace("4:", '"black_stake":')
-    file_data = file_data.replace("5:", '"blue_stake":')
-    file_data = file_data.replace("6:", '"purple_stake":')
-    file_data = file_data.replace("7:", '"orange_stake":')
-    file_data = file_data.replace("8:", '"gold_stake":')
+    # replaces difficulty naming convention for wins and losses in save file to a key/value pair format to enable json conversion
+    file_data = file_data.replace("1:", '"1":')
+    file_data = file_data.replace("2:", '"2":')
+    file_data = file_data.replace("3:", '"3":')
+    file_data = file_data.replace("4:", '"4":')
+    file_data = file_data.replace("5:", '"5":')
+    file_data = file_data.replace("6:", '"6":')
+    file_data = file_data.replace("7:", '"7":')
+    file_data = file_data.replace("8:", '"8":')
 
     file_data = json.loads(file_data)
+
     for joker in file_data["joker_usage"]:
         wins = 0
         losses = 0
@@ -43,8 +44,48 @@ def readFile():
         file_data["joker_usage"][joker]["wins"] = wins
         file_data["joker_usage"][joker]["losses"] = losses
 
-    print(file_data)
+    for deck in file_data["deck_usage"]:
+        wins = 0
+        losses = 0
+        for stake in file_data["deck_usage"][deck]["wins"]:
+            wins += file_data["deck_usage"][deck]["wins"][stake]
+        
+        for stake in file_data["deck_usage"][deck]["losses"]:
+            losses += file_data["deck_usage"][deck]["losses"][stake]
+        file_data["deck_usage"][deck]["wins"] = wins
+        file_data["deck_usage"][deck]["losses"] = losses
+
+    career = file_data["career_stats"]
+    print(file_data["name"])
+    career_stats = Career(
+        file_num = file_number,
+        file_name = file_data["name"],
+        cards_discarded = career["c_cards_discarded"],
+        hands_played = career["c_hands_played"],
+        dollars_earned = career["c_dollars_earned"],
+        cards_played = career["c_cards_played"],
+        planetarium_used = career["c_planetarium_used"],
+        wins = career["c_wins"],
+        shop_rerolls = career["c_shop_rerolls"],
+        losses = career["c_losses"],
+        tarots_bought = career["c_tarots_bought"],
+        shop_dollars_spent = career["c_shop_dollars_spent"],
+        planets_bought = career["c_planets_bought"],
+        vouchers_bought = career["c_vouchers_bought"],
+        tarot_reading_used = career["c_tarot_reading_used"],
+        rounds = career["c_rounds"],
+        jokers_sold = career["c_jokers_sold"],
+        face_cards_played = career["c_face_cards_played"],
+        playing_cards_bought = career["c_playing_cards_bought"]
+    )
+
+    db.session.add(career_stats)
+    db.session.commit()
+
+    # print(file_data)
     return jsonify({"message": "File Uploaded Successfully"}), 200
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 export const uploadFile = async data => {
     if(data.entries().next().value[1].size != 0){
-        let response = await fetch("http://127.0.0.1:5000/", {method: "POST", body: data}).then((rsp) => rsp.json());
+        let response = await fetch(`http://127.0.0.1:5000/upload_file/1`, {method: "POST", body: data}).then((rsp) => rsp.json());
         document.getElementById("file1").innerHTML = response.message;
     } else {
         document.getElementById("file1").innerHTML = "Please upload a profile.jkr file";
