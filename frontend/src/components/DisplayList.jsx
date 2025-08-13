f807631f8166bcf78e0f57ff0538350b16965a2f
Jay <jayrobertwhitaker@gmail.com>
2025-08-13 12:16:21 -0400

    Added styling to Joker display to mimic game and optimized joker.jsx

--- frontend/src/components/DisplayList.jsx
+++ frontend/src/components/DisplayList.jsx
@@ -6,19 +6,19 @@ import "../css/DisplayList.css"
 
 function DisplayList(props){
     if(props.list == null || props.item == null|| props.list.length == 0)
-        return <>
+        return <div id="displayBox">
             <p>Insert description of how the application works</p>
-        </>
+        </div>
     if(props.item == "career")
-        return <Career list={props.list}/>
+        return <div id="displayBox"><Career list={props.list}/></div>
     else if(props.item == "decks")
-        return <Deck list={props.list}/>
+        return <div id="displayBox"><Deck list={props.list}/></div>
     else if(props.item == "jokers")
-        return <Joker list={props.list}/>
+        return <div id="displayBox"><Joker list={props.list}/></div>
      else if(props.item == "hands" || props.item == "consumables" || props.item == "tarots" || props.item == "planets" || props.item == "spectrals" || props.item == "vouchers")
-        return <Other list={props.list} item={props.item}/>
+        return <div id="displayBox"><Other list={props.list} item={props.item}/></div>
     else {
-        return <p className="error">An error has occurred. Please reload the site.</p>
+        return <div id="displayBox"><p className="error">An error has occurred. Please reload the site.</p></div>
     }
 }
 
--- frontend/src/components/Joker.jsx
+++ frontend/src/components/Joker.jsx
@@ -1,3 +1,5 @@
+import "../css/Joker.css"
+
 function Joker(props){
     if(props.list.length == 0)
         return <></>
@@ -13,6 +15,31 @@ function Joker(props){
         return 0
     })
     
+    // sortedList.forEach((e) =>{
+    //     let joker = e.name
+    //     let count = e.count
+    //     let wins = e.wins
+    //     let losses = e.losses
+    //     joker = joker.replace(under_regex, " ")
+    //     while(joker.search(name_regex) >= 0){
+    //         joker = joker.replace(name_regex, char => char.toUpperCase())
+    //     }
+    //     joker = joker.replace(" The ", " the ")
+    //     joker = joker.replace(" In", "-In")
+    //     list.push(joker + `: Rounds: ${count} Wins: ${wins} Losses: ${losses}`)
+    // })
+    // return list.map((e, index) => {
+    //     if(index>=10) //remove and replace with proper page logic
+    //         return
+    //     let name = e.substring(0, e.indexOf(":"))
+    //     return <div className="joker">
+    //         <img src={`../images/jokers/${name}.webp`} alt={`Image of ${name}`} />
+    //         <p key={index} className="jokers">{e}</p>
+    //         <div ></div>
+    //     </div>
+    // })
+
+
     sortedList.forEach((e) =>{
         let joker = e.name
         let count = e.count
@@ -24,14 +51,21 @@ function Joker(props){
         }
         joker = joker.replace(" The ", " the ")
         joker = joker.replace(" In", "-In")
-        list.push(joker + `: Rounds: ${count} Wins: ${wins} Losses: ${losses}`)
+        e.name = joker
     })
-    return list.map((e, index) => {
-        let name = e.substring(0, e.indexOf(":"))
-        return <>
-            <p key={index} className="jokers">{e}</p>
+    return sortedList.map((e, index) => {
+        let max = sortedList[0].count
+        let barHeight = sortedList[index].count/max * 65
+        if(index>=10) //remove and replace with proper page logic
+            return
+        let name = sortedList[index].name
+        return <div key={index} className="joker">
             <img src={`../images/jokers/${name}.webp`} alt={`Image of ${name}`} />
-        </>
+            <p className="jokers">{`${name}: Rounds: ${sortedList[index].count} Wins: ${sortedList[index].wins} Losses: ${sortedList[index].losses}`}</p>
+            <div className="bar" style={{height: barHeight + '%'}}></div>
+        </div>
     })
+
+
 }
 export default Joker
