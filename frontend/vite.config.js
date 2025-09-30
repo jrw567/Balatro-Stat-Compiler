d5f7cf7200a6ead690a3ea53b1f56a9cb94c93ee
Jay <jayrobertwhitaker@gmail.com>
2025-09-30 13:51:27 -0400

    Added moved /frontend to tht esrc for main.jsx in index.html to fix the issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://balatro-stat-compiler-frontend.onrender.com/frontend'
+  base: 'https://balatro-stat-compiler-frontend.onrender.com'
 })
