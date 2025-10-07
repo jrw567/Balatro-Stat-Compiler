1b3917eeadb9b2ba11b43ac8f8cbe711de0da68b
Jay <jayrobertwhitaker@gmail.com>
2025-10-07 14:58:33 -0400

    Further debugging problems

--- backend/main.py
+++ backend/main.py
@@ -122,9 +122,9 @@ def readFile(file_number):
     )
     
     db.session.add(new_career)
-    return new_career.to_json()
 
     q = db.session.query(Career).filter(Career.file_num == TOTAL_FILE)
+    return q
     if(db.session.query(q.exists()).scalar()):
         for x in q:
             x.cards_discarded += career["cards_discarded"]
